89ab0d5eda748019ac60916afcd1c1a3402081df
HuangStanley050 <33140495+HuangStanley050@users.noreply.github.com>
2019-07-24 09:45:22 +1000

    printing out log for actions

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function() {
@@ -12,14 +13,24 @@ new Vue({
       this.monsterHealth = 100;
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      let damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player hits monster for " + damage
+      });
       if (this.checkWin()) {
         return;
       }
       this.monsterAttacks();
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      let damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player hits monster hard for " + damage
+      });
       if (this.checkWin()) {
         return;
       }
@@ -31,20 +42,30 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
-
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player healing for 10"
+      });
       this.monsterAttacks();
     },
     giveUp: function() {
       this.gameIsRunning = false;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
     monsterAttacks: function() {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      let damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+
       this.checkWin();
+      this.turns.unshift({
+        isPlayer: false,
+        text: "Monster hits player for " + damage
+      });
     },
     checkWin: function() {
       if (this.monsterHealth <= 0) {
