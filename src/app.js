902250c7ea5eb2279d0ef7c7c6bc71a062a56696
HuangStanley050 <33140495+HuangStanley050@users.noreply.github.com>
2019-07-24 09:25:19 +1000

    all actions finished

--- src/app.js
+++ src/app.js
@@ -25,7 +25,15 @@ new Vue({
       }
       this.monsterAttacks();
     },
-    heal: function() {},
+    heal: function() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+
+      this.monsterAttacks();
+    },
     giveUp: function() {
       this.gameIsRunning = false;
       this.playerHealth = 100;
